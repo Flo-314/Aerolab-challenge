5225d2e86369afc2fdd0381839d29a454fa503e3
Flo-314 <julianflo1916@gmail.com>
2022-02-01 20:46:20 -0300

    page hardcodeada

--- src/Components/Home/ProductsSection/components/Pager.jsx
+++ src/Components/Home/ProductsSection/components/Pager.jsx
@@ -1,8 +1,11 @@
 import {Flex, Image, Text, Button, Box} from "@chakra-ui/react";
+import {useState} from "react";
 
 import ChevronDownIcon from "../../../../Styling/UI Assets/assets/icons/chevron-default.svg";
 
 function Pager({handleMovePage}) {
+  const [page, SetPage] = useState(1);
+
   return (
     <Flex
       align="center"
@@ -23,6 +26,7 @@ function Pager({handleMovePage}) {
         maxWidth={"40px"}
         onClick={() => {
           handleMovePage(1);
+          SetPage(1);
         }}
       >
         <Image
@@ -42,7 +46,7 @@ function Pager({handleMovePage}) {
           Page
         </Text>
         <Text bg="brand.default" bgClip="text" display={"inline-block"}>
-          1 of 2
+          {page} of 2
         </Text>
       </Box>
 
@@ -54,6 +58,7 @@ function Pager({handleMovePage}) {
         maxWidth={"40px"}
         onClick={() => {
           handleMovePage(2);
+          SetPage(2);
         }}
       >
         <Image alt="chevron icon" maxHeight={"30px"} maxWidth={"15px"} src={ChevronDownIcon} />
